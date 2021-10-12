da4d9549391a7002a7c7085095dbc02f42262157
Megan Byers <meby3441@colorado.edu>
2021-10-11 19:36:19 -0600

    polishing homepage

---
+++ scripts/project.js
@@ -0,0 +1,79 @@
+
+//toggle open close of full case study window
+$('.toggle-case-study').click(function(){
+    clearInterval(timerID);
+    $('.case-study-window:eq('+active+')').toggle(50);
+    $('.case-study-wraper:eq('+active+')').css({"animation":"scale-up-bottom .4s cubic-bezier(.39,.575,.565,1.000) both", "animation-delay":"500ms"})
+    $( '.case-study-wrapper' ).toggle(300);
+    //reset iframes to prevent audio playing outside of case study
+    $('.case-study-iframe:eq('+active+')').attr("src", $('.case-study-iframe:eq('+active+')').attr("src"));
+    $('.additional-video-iframe-one:eq('+active+')').attr("src", $('.additional-video-iframe-one:eq('+active+')').attr("src"));
+    $('.additional-video-iframe-two:eq('+active+')').attr("src", $('.additional-video-iframe-two:eq('+active+')').attr("src"));
+
+})
+
+//  count number of slides  
+var theseProjects = document.getElementsByClassName('project');
+var active = 0;
+var counter = theseProjects.length;
+
+//add indexing dots 
+for (let i = 1; i < counter; i++) {
+$("#project-counter").append($(".counter").last().clone(true));
+}
+
+//  change slide  
+$('#next-slide').click(function(){
+    $('.project:eq('+active+')').delay(400).hide(0, function(){console.log("hiding!!")})
+    $('.counter:eq('+active+')').attr("src", "../assets/tan-textured-circle.svg")  
+
+     if(active >= counter - 1){active = 0;} 
+     else {active = active + 1;}
+    $('.project:eq('+active+')').show(250)
+    $('.counter:eq('+active+')').attr("src", "../assets/yellow-textured-circle.svg")  
+
+    clearInterval(timerID);
+})
+// delayed slides  
+var timerID = 
+    setInterval(function() {
+    $('.project:eq('+active+')').css("display", "none");
+    $('.counter:eq('+active+')').attr("src", "../assets/tan-textured-circle.svg")  
+
+        if(active >= counter - 1){active = 0;} 
+        else {active = active + 1;}
+    $('.project:eq('+active+')').css("display", "block");
+        $('.counter:eq('+active+')').attr("src", "../assets/yellow-textured-circle.svg")  
+
+    }, 10000);
+
+
+
+
+
+
+//  initial setup  
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+$('.project:eq('+active+')').css("display", "block")
+$('.counter:eq('+active+')').attr("src", "../assets/yellow-textured-circle.svg")  
+
+  
+
+});
+
+
+
+
+$('.back-to-top').click(function(){
+  $('.case-study-wrapper').scrollTop(0);
+})
+
+$('.expand-button').click(function() {
+  console.log('ya')
+    let elem = document.getElementById("fullcut-iframe");
+
+  elem.requestFullscreen()
+
+})
+
--- scripts/site.js
+++ scripts/site.js
@@ -19,6 +19,41 @@
     }
   }
 
+  function mediaQuery(x) {
+    if (x.matches) { 
+      $('h1').css({"font-size": "36px"})
+      $('#next-slide').css("width", "35px")
+      $('#next-slide img').css("margin-left", "0");
+      $('#summary-credits-wrapper').css({"display":"block"})
+      $('.case-study-summary').css("width", "100%")
+      $('.case-study-credits').css({"width":"100%", "margin-top":"17px"})
+      $('.case-study-toggle-arrow-wrapper').css("background-image","url('../assets/case-study-circle.svg')")
+      $('#bts-photos-grid').css('column-count', '1')
+  
+    } 
+    
+     else {
+      $('h1').css("font-size", "48px")
+      $('#next-slide').css("width", "50px")
+      $('#next-slide img').css("margin-left", "20px")
+  
+      //  case study  }
+      $('#summary-credits-wrapper').css({"display":"inline-flex"})
+      $('.case-study-summary').css("width", "calc(100% - 425px)")
+      $('.case-study-credits').css({"width":"400px", "margin-top":"0px"})
+      $('#bts-photos-grid').css('column-count', '3')
+      $(".case-study-arrow-wrapper").css("background-image","none")
+  
+  
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 600px)")
+  
+  mediaQuery(x) 
+  x.addListener(mediaQuery) 
+  
+
   // The event subscription that loads images when the page is ready
   document.addEventListener('DOMContentLoaded', loadAllImages);
 
