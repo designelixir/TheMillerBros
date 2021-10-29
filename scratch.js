d57b9eed020f48590b0f9fcccc8a6a61ca7210ab
Megan Byers <meby3441@colorado.edu>
2021-10-29 10:18:46 -0600

    stuff

--- scratch.js
+++ scratch.js
@@ -1,36 +1,8 @@
-function trackMouse(){$('.project').mousemove(followMouse);}
-
-function followMouse(e) {
-    if (e.pageX >= divRect.left && e.pageX <= divRect.right &&
-      e.pageY >= divRect.top && e.pageY <= divRect.bottom && followMouseBool) {
-        var xPos = e.pageX - itemRect.width / 2;
-        var yPos = e.pageY - itemRect.height / 2;
-        $('#traveling-button').animate({
-          'top':'0', 'left':'0', 'right':'0', 'bottom':'0'
-        }, 0, function(){
-          travelingbutton.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-        })
-    } else {
-
-        var small = document.getElementById('orange');
-        var big = document.getElementById('salmon'); 
-        var smallRect = small.getBoundingClientRect();
-        var bigRect = big.getBoundingClientRect();
-        var x = smallRect.right - 50 ;
-        var y = bigRect.bottom - 100 ;
-        $('#traveling-button').animate({
-          'top':'0', 'left':'0', 'right':'0', 'bottom':'0'
-        }, 0, function(){
-          travelingbutton.style.transform = "translate3d(" +  x + "px, " + y + "px, 0)";
-        })
-    }
-} 
 
 
 
-if (!mobileDevice) {
-    let beginMouseTrack = setTimeout(trackMouse, 1000);
-  } 
+
+
 
   function bringTheDivs(){
     console.log('called bring the divs')
